018d49153b0cc11b5327ea558dc4a6cf91655231
composerben <56130322+composerben@users.noreply.github.com>
2021-05-20 12:38:57 -0700

    finish frontend setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import App from "./App";
 
 import configureStore from "./store";
@@ -10,6 +11,9 @@ import configureStore from "./store";
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
