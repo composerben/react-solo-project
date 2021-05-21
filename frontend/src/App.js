eb3e7bd7c874f504ce678fa1fa13a7d5af59da29
composerben <56130322+composerben@users.noreply.github.com>
2021-05-21 13:21:21 -0700

    implement logout functionality and profile button that displays based on login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,16 +14,19 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoggedIn(true));
   }, [dispatch]);
   return (
-    isLoggedIn && (
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-    )
+    <>
+      <Navigation isLoggedIn={isLoggedIn} />
+      {isLoggedIn && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 
---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import * as sessionActions from "../../store/session";
+
+const ProfileButton = () => {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+    document.addEventListener("click", closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+  return (
+    <div className="profile-button">
+      <button onClick={openMenu}>
+        <i class="far fa-user"></i>
+      </button>
+      {showMenu && (
+        <ul className="profile-dropdown">
+          {/* <li>{user.username}</li>
+          <li>{user.email}</li> */}
+          <li>
+            <button onClick={logout}>Log Out</button>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ProfileButton;
