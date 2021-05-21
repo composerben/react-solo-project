f4c6acb61d6236f9348afc710558b343e477865a
composerben <56130322+composerben@users.noreply.github.com>
2021-05-21 12:15:33 -0700

    fix typo for logout action type

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,7 +1,7 @@
 import { csrfFetch } from "./csrf";
 
 const SET_SESSION = "session/SET_SESSION";
-const REMOVE_SESSION = "session/REMOVE_SESSIOn";
+const REMOVE_SESSION = "session/REMOVE_SESSION";
 
 const initialState = {
   user: null,
@@ -42,6 +42,14 @@ export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch("/api/session");
   const data = await response.json();
   dispatch(setUser(data.user));
+  return response;
+};
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch("/api/session", {
+    method: "DELETE",
+  });
+  dispatch(removeUser());
   return response;
 };
 
