1cac6f84d40d5164124d114d1e56a3768b247351
composerben <56130322+composerben@users.noreply.github.com>
2021-05-21 17:55:42 -0700

    initial navbar styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -13,18 +13,24 @@ const Navigation = ({ isLoggedIn }) => {
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink className="navlink" to="/login">
+          Log In
+        </NavLink>
+        <NavLink className="navlink" to="/signup">
+          Sign Up
+        </NavLink>
       </>
     );
   }
 
   return (
-    <nav>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-      {isLoggedIn && sessionLinks}
+    <nav className="navbar">
+      <div className="navbar__navlinks-container">
+        <NavLink className="navlink" exact to="/">
+          Home
+        </NavLink>
+        {isLoggedIn && sessionLinks}
+      </div>
     </nav>
   );
 };
